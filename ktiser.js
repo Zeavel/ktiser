ffbf9a72e8a392168d2fd0339d48934893f3aff0
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-10-30 18:44:16 +0400

    Update ktiser.js

--- ktiser.js
+++ ktiser.js
@@ -3,31 +3,21 @@ const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {
   polling: true
 });
-var prefix = "."
-function commandIs(str, msg){
 
- 
-  return msg.text.toString().toLowerCase().startsWith(prefix + str);
-}
 bot.on('message', (msg) => {
     
   //anything
      
 });
 bot.on('message', (msg) => {
-    function send(str)
-    {
-      bot.sendMessage(msg.chat.id, str)
-    }
+  console.log(msg.chat.id)
     var Hi = "hi";
     if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
     bot.sendMessage(msg.chat.id,"Hello dear user");
     
     }
-    if(commandIs("Da", message))
-    {
-     send("test")
-    }
+    
    
       
     });
+    
