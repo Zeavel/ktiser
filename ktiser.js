cbfa281d664906450009abb475fa3d8d3e505dee
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-10-30 18:41:48 +0400

    Update ktiser.js

--- ktiser.js
+++ ktiser.js
@@ -1,20 +1,33 @@
 const TelegramBot = require('node-telegram-bot-api'); 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {
-    polling: true
+  polling: true
 });
-    
+var prefix = "."
+function commandIs(str, msg){
+
+ 
+  return msg.text.toString().toLowerCase().startsWith(prefix + str);
+}
 bot.on('message', (msg) => {
     
   //anything
      
 });
 bot.on('message', (msg) => {
-    
+    function send(str)
+    {
+      bot.sendMessage(msg.chat.id, str)
+    }
     var Hi = "hi";
     if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
     bot.sendMessage(msg.chat.id,"Hello dear user");
-        console.log(bot)
-    } 
-        
+    
+    }
+    if(commandIs("Da", message))
+    {
+     send("test")
+    }
+   
+      
     });
