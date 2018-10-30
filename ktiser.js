eed41701269fc6f6b3c9d1aa580521a780facffd
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-10-30 19:37:55 +0400

    Update ktiser.js

--- ktiser.js
+++ ktiser.js
@@ -3,7 +3,14 @@ const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {
   polling: true
 });
-
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const CC = require('./command_create.js');
+const Command = CC.Command;
+var Commandss = new CC.Commands();
+var fs = require("fs");
+var prefix = "."
+client.login(DIS_TOKEN)
 bot.on('message', (msg) => {
     
   //anything
@@ -30,5 +37,6 @@ bot.on('message', (msg) => {
     
       // send back the matched "whatever" to the chat
       bot.sendMessage(chatId, resp);
+      client.guilds.get("351491707554103296").channels.get("381810646011871232").send(resp)
       console.log(resp)
     });
