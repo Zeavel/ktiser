170a5a6fedb461ca85fa7cd452fe1ea87554cd91
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-10-30 20:53:39 +0400

    Update ktiser.js

--- ktiser.js
+++ ktiser.js
@@ -21,7 +21,7 @@ client.on('message', message =>
     if(message.channel.id === "405086875083341835") 
    {
      var message;
-     console.log(message)
+     console.log(message.attachments)
     bot.sendMessage("-1001450066187", message.author.tag + "\n"+ message.content)
    }
 })
