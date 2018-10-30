29fbbb55e1805f70ed035fdd2dad702372abf2d8
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-10-30 18:36:22 +0400

    Update ktiser.js

--- ktiser.js
+++ ktiser.js
@@ -14,7 +14,7 @@ bot.on('message', (msg) => {
     var Hi = "hi";
     if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
     bot.sendMessage(msg.chat.id,"Hello dear user");
-        console.log(msg)
+        console.log(bot)
     } 
         
     });
