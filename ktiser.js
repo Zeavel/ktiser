96e958a6ddcbd3e1e1cf3a6c4d43e65820639f87
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-10-30 18:17:39 +0400

    Update ktiser.js

--- ktiser.js
+++ ktiser.js
@@ -1,58 +1,17 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const CC = require('./command_create.js');
-const Command = CC.Command;
+const TelegramBot = require('node-telegram-bot-api'); 
+const token = process.env.BOT_TOKEN;
 
-
-
-var Commandss = new CC.Commands();
-var fs = require("fs");
-
-function commandIs(str, msg){
-    return msg.content.toLowerCase().startsWith("." + str);
-}
-
-function pluck(array) {
-    return array.map(function(item) { return item["name"]; });
-}
-
-function hasRole(mem, role)
-{
-    if (pluck(mem.roles).includes(role))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-
-
-client.on('message', message => {
-     if(commandIs("tst", message))
-    {
-        const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
+    
+bot.on('message', (msg) => {
+    
+  //anything
+     
 });
-
-client.connect();
-
-client.query('CREATE TABLE cheyka(ID CHAR(10) NOT NULL Primary key, Custom_name CHAR(25) NOT NULL)', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-        message.channel.send("test access")
-    }
-  });
-
-
-
-client.login(process.env.BOT_TOKEN);
+bot.on('message', (msg) => {
+    
+    var Hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Hello dear user");
+    } 
+        
+    });
