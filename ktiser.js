b301f9d1ad78a3d6c1685b00c65d8f6842fe0c8e
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-10-30 21:20:24 +0400

    Update ktiser.js

--- ktiser.js
+++ ktiser.js
@@ -25,14 +25,18 @@ client.on('message', message =>
      var args1;
      var args2;
      var id;
-     if(message.content.includes("<@")
+     if(message.content.includes("<@"))
         {
         args1 = message.content.split("<@")
         args2 = args1[1].split(">")
         id = args2[0]
      chet = message.content.replace("<@"+id+">", "@"+message.channel.members.get(id).user.username)
+          messag = chet + message.attachments.map(h => h.url).toString()
         }
-     messag = chet + message.attachments.map(h => h.url).toString()
+     else
+     {
+       messag = message.content + message.attachments.map(h => h.url).toString()
+     }
      
      
     bot.sendMessage("-1001450066187", message.author.tag + "\n"+ messag)
