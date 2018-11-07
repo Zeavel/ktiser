6d18d95c2d1a51fccdff2bc3991067d979e761b4
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-11-07 19:54:34 +0400

    Update ktiser.js

--- ktiser.js
+++ ktiser.js
@@ -69,7 +69,7 @@ bot.on('message', (msg) => {
       // send back the matched "whatever" to the chat
       bot.sendMessage(chatId, resp);
       client.guilds.get("351491707554103296").channels.get("381810646011871232").send(resp)
-     bot.getChatAdministrators("-1001450066187").then(console.log)
+    console.log(msg)
     });
  bot.onText(/\/dis (.+)/, (msg, match) => {
       // 'msg' is the received Message from Telegram
